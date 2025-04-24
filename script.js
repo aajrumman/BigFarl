9642db3ab2751cdd505a70ae9dbea145fd1cec1d
AAJ Rumman <aajrumman@gmail.com>
2025-04-24 17:27:53 +0600

    intro

---
+++ script.js
@@ -0,0 +1,6 @@
+// Show main content after 6 seconds (intro duration)
+setTimeout(() => {
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+  }, 6000);
+  
