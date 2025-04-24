b9f86822d0a925610ab478e44088887bf1c858c3
AAJ Rumman <aajrumman@gmail.com>
2025-04-24 17:53:06 +0600

    intro

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Show main content after 6 seconds (intro duration)
-setTimeout(() => {
-    document.getElementById('preloader').style.display = 'none';
-    document.getElementById('main-content').style.display = 'block';
-  }, 6000);
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      document.getElementById('intro').style.display = 'none';
+    }, 10000); // Hide intro after 10s
+  });
   
